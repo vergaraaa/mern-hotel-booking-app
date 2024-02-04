f16ffd5c45ee2d94a1dbd2f707b927d6d2cd3ff1
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-04 16:20:13 +0100

    feat: connect to db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,24 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
 import 'dotenv/config';
+import cors from 'cors';
+import morgan from 'morgan';
+import mongose from 'mongoose';
+import express, { Request, Response } from 'express';
 
+// db connection
+mongose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => console.log('db connected'));
+
+// init app
 const app = express();
 
 // settings
 app.set('port', 3000 || process.env.PORT);
 
 // middlewares
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 // test route
 app.get("/api/test", async(req: Request, res: Response) => {
