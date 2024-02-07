805f62eb968af2dafe359b7db9d1ebdbe3982c7f
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-07 20:02:39 +0100

    feat: auth login test

---
+++ e2e/tests/auth.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173/";
+
+test("should allow user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  // get sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  // fill form
+  await page.locator("[name=email]").fill("vergara@gmail.com");
+  await page.locator("[name=password]").fill("123456");
+
+  // click login button
+  await page.getByRole("button", { name: "Sign In" }).click();
+
+  // assertions
+  await expect(page.getByText("Login successful")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
