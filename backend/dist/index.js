a4170906d6dc1f6d701dd6494b576b7387312461
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-08 18:43:12 +0100

    feat: serve frontend in backend statically

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const morgan_1 = __importDefault(require("morgan"));
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
@@ -12,12 +13,13 @@ const auth_1 = __importDefault(require("./routes/auth"));
 const users_1 = __importDefault(require("./routes/users"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 // db connection
-mongoose_1.default.connect(process.env.MONGODB_CONNECTION_STRING)
-    .then(() => console.log('db connected'));
+mongoose_1.default
+    .connect(process.env.MONGODB_CONNECTION_STRING)
+    .then(() => console.log("db connected"));
 // init app
 const app = (0, express_1.default)();
 // settings
-app.set('port', 3000 || process.env.PORT);
+app.set("port", 3000 || process.env.PORT);
 // middlewares
 app.use((0, cors_1.default)({
     origin: process.env.FRONTEND_URL,
@@ -27,9 +29,10 @@ app.use((0, morgan_1.default)("dev"));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
 // routes
-app.use('/api/users', users_1.default);
-app.use('/api/auth', auth_1.default);
+app.use("/api/users", users_1.default);
+app.use("/api/auth", auth_1.default);
 // server lostening
 app.listen(app.get("port"), () => {
     console.log("server running on port", app.get("port"));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,43 @@
-import 'dotenv/config';
-import cors from 'cors';
-import morgan from 'morgan';
-import express from 'express';
-import mongose from 'mongoose';
-import authRoutes from './routes/auth';
-import userRoutes from './routes/users';
-import cookieParser from 'cookie-parser';
+import "dotenv/config";
+import cors from "cors";
+import path from "path";
+import morgan from "morgan";
+import express from "express";
+import mongose from "mongoose";
+import authRoutes from "./routes/auth";
+import userRoutes from "./routes/users";
+import cookieParser from "cookie-parser";
 
 // db connection
-mongose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(() => console.log('db connected'));
-
+mongose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("db connected"));
 
 // init app
 const app = express();
 
-
 // settings
-app.set('port', 3000 || process.env.PORT);
-
+app.set("port", 3000 || process.env.PORT);
 
 // middlewares
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-}));
+  })
+);
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 // routes
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // server lostening
 app.listen(app.get("port"), () => {
-    console.log("server running on port", app.get("port"));
+  console.log("server running on port", app.get("port"));
 });
