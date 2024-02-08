73badef699765cef721ecbda571fd66002911d13
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-08 18:48:06 +0100

    change when vite api base url doesnt exist

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,65 +1,65 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
-export const register = async(formData: RegisterFormData) => {
-    const request = await fetch(`${API_BASE_URL}/api/users/register`, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData)
-    });
+export const register = async (formData: RegisterFormData) => {
+  const request = await fetch(`${API_BASE_URL}/api/users/register`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
 
-    const data = await request.json();
+  const data = await request.json();
 
-    if(data.status === "failure") {
-        throw new Error(data.message);
-    }
-}
+  if (data.status === "failure") {
+    throw new Error(data.message);
+  }
+};
 
-export const login = async(formData: SignInFormData) => {
-    const request = await fetch(`${API_BASE_URL}/api/auth/login`, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData)
-    });
+export const login = async (formData: SignInFormData) => {
+  const request = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
 
-    const data = await request.json();
+  const data = await request.json();
 
-    if(data.status === "failure") {
-        throw new Error(data.message);
-    }
-}
+  if (data.status === "failure") {
+    throw new Error(data.message);
+  }
+};
 
-export const validateToken = async() => {
-    const request = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+export const validateToken = async () => {
+  const request = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if(!request.ok) {
-        throw new Error("Token invalid");
-    }
+  if (!request.ok) {
+    throw new Error("Token invalid");
+  }
 
-    return await request.json();
-}
+  return await request.json();
+};
 
-export const logout = async() => {
-    const request = await fetch(`${API_BASE_URL}/api/auth/logout`, {
-        method: "POST",
-        credentials: "include",
-    });
+export const logout = async () => {
+  const request = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+    method: "POST",
+    credentials: "include",
+  });
 
-    if(!request.ok) {
-        throw new Error("Error during logout");
-    }
-}
+  if (!request.ok) {
+    throw new Error("Error during logout");
+  }
+};
