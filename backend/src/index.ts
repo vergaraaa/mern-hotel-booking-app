d9d0c9a6cdecfacb5630510a6cda122e6c795be4
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-09 12:36:58 +0100

    setup: cloudinary

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,11 +7,19 @@ import mongose from "mongoose";
 import authRoutes from "./routes/auth";
 import userRoutes from "./routes/users";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 // db connection
 mongose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("db connected"));
+
+// cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // init app
 const app = express();
@@ -30,7 +38,6 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 // routes
