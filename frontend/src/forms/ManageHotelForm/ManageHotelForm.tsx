7482b0af5fbfefd5aecc89c99de1705876bf7001
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-19 11:26:55 +0100

    feat: images section

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -3,6 +3,7 @@ import DetailsSection from "./DetailsSection";
 import TypesSection from "./TypesSection";
 import FacilitiesSection from "./FacilitiesSection";
 import GuestsSection from "./GuestsSection";
+import ImagesSection from "./ImagesSection";
 
 export type HotelFormData = {
   name: string;
@@ -28,6 +29,7 @@ const ManageHotelForm = () => {
         <TypesSection />
         <FacilitiesSection />
         <GuestsSection />
+        <ImagesSection />
       </form>
     </FormProvider>
   );
